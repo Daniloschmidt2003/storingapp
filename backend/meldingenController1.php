91af713871716239e31109f18a2d9e747fe6d7ec
Daniloschmidt2003 <129153339+Daniloschmidt2003@users.noreply.github.com>
2023-09-27 14:50:09 +0200

    dw
    wef

--- backend/meldingenController1.php
+++ backend/meldingenController1.php
@@ -4,45 +4,50 @@
 $attractie = $_POST['attractie'];
 $capaciteit = $_POST['capaciteit']; 
 $melder = $_POST['melder'];
-$type = $_POST['group'];
-$news = $_POST['news'];
-$prioriteit = $_POST['prioriteit'];
-echo $attractie . " / " . $capaciteit . " / " . $melder . " / " . $type . " / " .$prioriteit;
-
-//1. Verbinding
-require_once 'conn.php';
-
-//2. Query
-$query = " INSERT INTO meldingen (attractie, type, capaciteit, melder)
-VALUES(:attractie, :type, :capaciteit, :melder, prioriteit)";
-//3. Prepare
-$statement = $conn->prepare($query);
-
-//4. Execute
-$items = $statement->fetchAll(PDO::FETCH_ASSOC);
-header("Location:../meldingen/index.php?msg=Meldingopgeslagen");
-$statement ->execute([
-    ":attractie" => $attractie, 
-    ":type" => $type,
-    ":capaciteit" => $capaciteit,
-    ":melder" => $melder,
-    ":prioriteit" => $prioriteit,
-]);
-
-if(isset($_POST['$proiriteit']))
+$type = $_POST['type'];
+$overig = $_POST['overig'];
+if(isset($_POST['prioriteit']))
 {
     $prioriteit = true;
 }
 else
 {
-    $prioriteit= false;
+    $prioriteit = false;
 }
 
-if(isset($_POST['$news']))
+if(isset($_POST['newsletter']))
 {
-    $news = true;
+    $newsletter = true;
 }
 else
 {
-    $news = false;
+    $newsletter = false;
 }
+echo $attractie . " / " . $type . " / " . $capaciteit . " / " . $melder . " / " . $prioriteit . " / " . $overig;
+
+//1. Verbinding
+require_once 'conn.php';
+
+//2. Query
+$query = "INSERT INTO meldingen (attractie, type, capaciteit, melder, prioriteit, overige_info)
+VALUES(:attractie, :type, :capaciteit, :melder, :prioriteit, :overige_info)";
+//3. Prepare
+$statement = $conn->prepare($query);
+
+$statement->execute([
+    ":attractie" => $attractie,
+    ":capaciteit" => $capaciteit,
+    ":melder" => $melder,
+    ":type" => $type,
+    ":prioriteit" => $prioriteit,
+    ":overige_info" => $overig
+]);
+
+//4. Execute
+
+
+
+
+$items = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+header("Location:../meldingen/index.php?msg=Meldingopgeslagen");
